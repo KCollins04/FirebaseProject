01f7acb40453051927f62bb98eaea54a958d0cd4
ok <kelsey.collins23@d155.org>
2023-01-19 08:13:37 -0600

    save info to firebase

--- FirebaseProject/ViewController.swift
+++ FirebaseProject/ViewController.swift
@@ -10,16 +10,29 @@ import FirebaseCore
 import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var nameOutlet: UITextField!
+    var ref: DatabaseReference!
+    var words = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //connects to Firebase
+        ref = Database.database().reference()
+        ref.child("today").observe(.childAdded){ snapshot in
+           let word = snapshot.value as! String
+           self.words.append(word)
+           
+            
+        }
     }
 
     
     @IBAction func addButton(_ sender: UIButton) {
-        
+        //set outlet to a value
+        let word = nameOutlet.text!
+        //send the word to firebase called todays
+        ref.child("today").childByAutoId().setValue(word)
         
     }
     
